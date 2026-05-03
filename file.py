c3afe41a3c5de264993893c2cb162a4144efb81f
Navaneethajogu <154861163+Navaneethajogu@users.noreply.github.com>
2026-05-03 12:47:10 +0530

    Add files via upload

---
+++ file.py
@@ -0,0 +1,23 @@
+
+
+# with open('loky.text','r') as file:         #no nee to close the file
+#     print(file.read())
+    
+    
+# with open('lo.txt','a') as file:         #no nee to close the file
+#     print(file.write('\nhow are you'))
+
+with open('lok.txt','a') as file:         #no nee to close the file
+    print(file.write('\nhow are you'))
+
+
+#file=open("loky.text",'r')
+#print(file.read())
+#print(file.readline(6))
+#print(file.readlines())
+
+# file=open("lo.txt",'w')
+# print(file.write('hello loky'))
+
+
+
