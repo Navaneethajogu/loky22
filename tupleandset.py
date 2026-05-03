4af0cc3d70f121ff3f1aac067351f653e60fdbc0
Navaneethajogu <154861163+Navaneethajogu@users.noreply.github.com>
2026-05-03 15:07:05 +0530

    Add files via upload

---
+++ tupleandset.py
@@ -0,0 +1,29 @@
+### tuple methods #####
+t=(1,2,3,4,5,)
+#print(t)
+#print(len(t))
+#print(max(t))
+#print(min(t))
+#print(sum(t))
+#print(all((t)))
+#print(any(t))
+#print(t.index(3))
+#print(t.count(5))
+
+####################################################
+
+#### set methods ####
+s=[2,3,4,5,6]
+s=set(s)
+#print(s)
+#print(len(s))
+#print(max(s))
+#print(min(s))
+#print(sum(s))
+#print(all(s))
+#print(any(s))
+#print(s.add(7))
+#print(s.remove(3))
+#print(s.discard(4))
+#print(s.pop())
+#print(s.clear())
