78a78e88fa8ad9fa0b72fcf6e11d89cf4cc55542
Navaneethajogu <154861163+Navaneethajogu@users.noreply.github.com>
2026-05-10 10:30:11 +0530

    Add files via upload

---
+++ python questions.py
@@ -0,0 +1,3 @@
+python operators,python datatypes,python control flow,python function
+python higher order functions ,python list comprehensions,python generators,python decorators
+problems:primenumber,factorial,fibonacci,revers string,check palindrome,findlargest numer in list,sort list
